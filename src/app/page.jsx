2e2edf8cd98b8e654832713dca5c176944246ac4
zaki <zacky@ipayafrica.com>
2024-02-07 15:07:05 +0300

    Added a notify alert component to orchestrate logic to display an alert component with one FN call :)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,21 +3,19 @@
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "../lib/redux/feutures/counterSlice";
 
-<<<<<<< HEAD:src/app/page.js
-import ProTip from "@/components/ProTip";
+
 import { handleFetchData, handlePostData } from "@/apis";
-=======
 import ProTip from "@/components/pro-tip";
->>>>>>> 3da651e09dde78122a5b5fb65a845cb348a6ca3c:src/app/page.jsx
+import { decrement, increment } from "../lib/redux/feutures/counterSlice";
+import { useNotifyAlertCtx } from "@/components/notify-alert/notify-alert-context";
 
 export default function Home() {
   const count = useSelector((state) => state.counter.value); // Access the counter state
   //useDispatch updates the store with the state from a component, as defined by your logic inside the counterslice.js
   const dispatch = useDispatch();
+  const setAlertMessage = useNotifyAlertCtx();
 
   handleFetchData("/users", true)
     .then((responseData) => {
@@ -60,6 +58,23 @@ export default function Home() {
           <h1>Counter: {count}</h1> {/* Display the counter state */}
           <button onClick={() => dispatch(increment())}>Increment</button>
           <button onClick={() => dispatch(decrement())}>Decrement</button>
+          <Box sx={{ backgroundColor: "aliceblue" }}>
+            <p>
+              The button below👇 will trigger an alert to open. To customize the
+              displayed alert, see code in <code>src/app/page.jsx</code> line{" "}
+              <code>47</code>
+            </p>
+            <button
+              onClick={() =>
+                setAlertMessage("An Alert message for youz! :)", {
+                  closeOnClickAway: false,
+                  type: "info",
+                })
+              }
+            >
+              trigger alert
+            </button>
+          </Box>
         </Box>
       </Box>
     </Container>
---
+++ src/components/notify-alert/notify-alert-context/index.jsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { createContext, useContext } from "react";
+
+import NotifyAlert from "../index.jsx";
+import { useNotifyAlert } from "../useNotifyAlert.jsx";
+
+/**
+ * @typedef {import("../types.js").alertmsg} alertMsg
+ * @typedef {import("../types.js").alertdesc} alertDesc
+ */
+
+const alertConfig = {
+  /**
+   * @type {boolean}
+   */
+  alertOpen: false,
+  /**
+   * @type {string}
+   */
+  alertMessage: "",
+  /**
+   * @type {?function}
+   */
+  closeFn: undefined,
+  /**
+   * @type {string}
+   */
+  alertType: "info",
+  /**
+   * @type {?number}
+   */
+  openDuration: null,
+  /**
+   * Controls the content/behaviour of global alert component
+   * @param {alertMsg} msg
+   * @param {...alertDesc} [desc]
+   * @returns {void}
+   */
+  setAlertMessage: (msg, desc) => {},
+};
+
+const NotifyAlertContext = createContext(alertConfig);
+
+export default function NotifyAlertProvider({ children }) {
+  const { setAlertMessage, alertProps } = useNotifyAlert();
+
+  return (
+    <NotifyAlertContext.Provider value={{ setAlertMessage }}>
+      <NotifyAlert {...alertProps} />
+      {children}
+    </NotifyAlertContext.Provider>
+  );
+}
+
+export function useNotifyAlertCtx() {
+  const context = useContext(NotifyAlertContext);
+
+  if (context === undefined) {
+    throw new Error(
+      "useNotifyAlertCtx must be used within NotifyAlertProvider"
+    );
+  }
+  return context.setAlertMessage;
+}
