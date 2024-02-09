aa638322c17464b1607de32866da358bb30149f5
SHIRLEY-GITHINJI <shirley@ipayafrica.com>
2024-02-09 10:22:33 +0300

    login page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,42 +1,9 @@
-"use client"; //this is a client side component
+"use client";
 
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import Login from "./auth/login/page";
 
-import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "../lib/redux/feutures/counterSlice";
-
-import ProTip from "@/components/pro-tip";
-
-export default function Home() {
-  const count = useSelector((state) => state.counter.value); // Access the counter state
-
-  //useDispatch updates the store with the state from a component, as defined by your logic inside the counterslice.js
-  const dispatch = useDispatch();
-
-  return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant="h4" component="h1">
-          Admin dashboard
-        </Typography>
-        <ProTip />
-
-        <Box>
-          <h1>Counter: {count}</h1> {/* Display the counter state */}
-          <button onClick={() => dispatch(increment())}>Increment</button>
-          <button onClick={() => dispatch(decrement())}>Decrement</button>
-        </Box>
-      </Box>
-    </Container>
-  );
+function Home() {
+  return <Login />;
 }
+
+export default Home;
