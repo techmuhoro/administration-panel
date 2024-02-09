0a225bc464055fce1a2bd1a80428504e48fbbdbf
SHIRLEY-GITHINJI <shirley@ipayafrica.com>
2024-02-09 09:13:34 +0300

    update custom input

--- src/atoms/FormikCustomInput/index.jsx
+++ src/atoms/FormikCustomInput/index.jsx
@@ -1,94 +1,78 @@
-import { Field, ErrorMessage, getIn } from "formik";
-import PropTypes from "prop-types";
+import { useState } from "react";
 
-import { TextField, MenuItem } from "@mui/material";
+import { Field, ErrorMessage, getIn } from "formik";
+
+import { TextField, InputAdornment, MenuItem, IconButton } from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 const FormikCustomInput = ({
-  type,
-  label,
-  variant,
   name,
-  multiline,
-  required,
-  error,
-  helperText,
+  type,
   select,
+  icon,
   selectItem,
-  onChange,
-  onBlur,
-  shrink,
-  disabled,
-  autoFocus,
-  ...restProps
+  placeholder,
+  ...rest
 }) => {
+  const [visibility, setVisibility] = useState(false);
+
+  const typeChangeHandler = () => {
+    setVisibility((prev) => !prev);
+  };
   return (
     <Field name={name}>
-      {({ field, form }) => {
-        return (
-          <TextField
-            {...field}
-            id={label}
-            variant={variant}
-            type="text"
-            name={name}
-            onChange={onChange !== null ? onChange : form.handleChange}
-            onBlur={onBlur !== null ? onBlur : form.handleBlur}
-            required={required}
-            helperText={<ErrorMessage name={name} />}
-            error={Boolean(
-              getIn(form.touched, name) && getIn(form.errors, name)
-            )}
-            placeholder={label}
-            size="medium"
-            fullWidth
-            multiline={multiline}
-            select={select}
-            {...restProps}
-          >
-            {select &&
-              selectItem.map((option) => (
-                <MenuItem key={option.key} value={option.value}>
-                  {option.key}
-                </MenuItem>
-              ))}
-          </TextField>
-        );
-      }}
+      {({ field, form }) => (
+        <TextField
+          {...field}
+          select={select}
+          fullWidth
+          name={name}
+          size="small"
+          placeholder={placeholder}
+          type={type !== "password" ? type : visibility ? "text" : "password"}
+          helperText={<ErrorMessage name={name} />}
+          error={Boolean(getIn(form.touched, name) && getIn(form.errors, name))}
+          InputProps={{
+            endAdornment: type === "password" && (
+              <InputAdornment>
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={typeChangeHandler}
+                >
+                  {visibility ? (
+                    <Visibility sx={{ fontSize: 18 }} />
+                  ) : (
+                    <VisibilityOff sx={{ fontSize: 18 }} />
+                  )}
+                </IconButton>{" "}
+              </InputAdornment>
+            ),
+            sx: {
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: "#DADEF0", // change the border color here
+              },
+
+              input: {
+                "&::placeholder": {
+                  color: "#00000",
+                  fontSize: 13,
+                },
+              },
+            },
+          }}
+          {...rest}
+        >
+          {select &&
+            selectItem.map((option) => (
+              <MenuItem key={option.key} value={option.value}>
+                {option.key}
+              </MenuItem>
+            ))}
+        </TextField>
+      )}
     </Field>
   );
-};
-
-FormikCustomInput.defaultProps = {
-  label: "",
-  multiline: false,
-  variant: "outlined",
-  required: false,
-  error: false,
-  helperText: "",
-  select: false,
-  selectItem: [],
-  onChange: null,
-  onBlur: null,
-  shrink: false,
-  type: "text",
-  disabled: false,
-};
-
-FormikCustomInput.propTypes = {
-  type: PropTypes.string,
-  label: PropTypes.string,
-  onChange: PropTypes.func,
-  onBlur: PropTypes.func,
-  name: PropTypes.string.isRequired,
-  multiline: PropTypes.bool,
-  variant: PropTypes.string,
-  required: PropTypes.bool,
-  error: PropTypes.bool,
-  helperText: PropTypes.string,
-  select: PropTypes.bool,
-  selectItem: PropTypes.arrayOf(PropTypes.shape({})),
-  shrink: PropTypes.bool,
-  disabled: PropTypes.bool,
 };
 
 export default FormikCustomInput;
