77f0cfd51ad8a68343e78bb033bbf225d4235526
Albert <albert@ipayafrica.com>
2024-02-14 11:17:07 +0300

    add demo slice

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 "use client"; //this is a client side component
 
+import { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
@@ -7,36 +8,75 @@ import Box from "@mui/material/Box";
 import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "../lib/redux/feutures/counterSlice";
 
-import ProTip from "@/components/ProTip";
-import { handleFetchData, handlePostData } from "@/apis";
+import ProTip from "../components/pro-tip";
+import { handleFetchData } from "@/apis";
+
+import {
+  getError,
+  getLoading,
+  fetchPosts,
+  getPost,
+} from "../lib/redux/demo/getdemoSlice";
+
+import {
+  getPostdemoLoading,
+  getpostdemoError,
+  getPostdemoResponse,
+  sendPost,
+} from "../lib/redux/demo/postdemoSlice";
 
 export default function Home() {
   const count = useSelector((state) => state.counter.value); // Access the counter state
-  //useDispatch updates the store with the state from a component, as defined by your logic inside the counterslice.js
+
+  const [value, setValue] = useState("");
+  const [titleValue, setTitleValue] = useState("");
+  const getPosts = useSelector(getPost);
+  const loading = useSelector(getLoading);
+  const error = useSelector(getError);
+
   const dispatch = useDispatch();
 
-  handleFetchData("/users", true)
-    .then((responseData) => {
-      console.log("Response Data:", responseData);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-
-  let payload = {
-    email: "string",
-    password: "string",
-  };
-  // handlePostData("login", payload, true)
-  //   .then((responseData) => {
-  //     console.log("Response Data:", responseData);
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error:", error);
-  //   });
-
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, []);
   return (
     <Container maxWidth="lg">
+      <div className="top">
+        <label htmlFor="title">
+          Title:
+          <input
+            type="text"
+            name="title"
+            value={titleValue}
+            onChange={(e) => setTitleValue(e.target.value)}
+          />
+        </label>
+        <label htmlFor="body">
+          Body:
+          <input
+            type="text"
+            name="body"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </label>
+        <button
+          onClick={() =>
+            dispatch(sendPost({ title: titleValue, body: value, userId: "1" }))
+          }
+        >
+          submit
+        </button>
+      </div>
+      {loading ? "loading ..." : null}
+      <div>
+        {getPosts.map((post, index) => (
+          <div className="single-post" key={index}>
+            <h4>{post.title}</h4>
+            <h6>{post.body}</h6>
+          </div>
+        ))}
+      </div>
       <Box
         sx={{
           my: 4,
