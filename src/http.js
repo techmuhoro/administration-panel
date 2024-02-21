40604ffd6b544f0b283f5274c1343b97244e80a7
Albert <albert@ipayafrica.com>
2024-02-21 18:31:40 +0300

    update user-details

--- src/http.js
+++ src/http.js
@@ -9,15 +9,49 @@ const http = axios.create({
   baseURL,
   headers: {
     "Content-Type": "application/json",
-    "X-powered-by": "z_axios",
+    "X-powered-by": "axios",
   },
   timeout: 10000,
 });
 
+// http.interceptors.request.use(
+//   async (config) => {
+//     // `includeAuthorization` is a custom config. It is checked to let axios know when we need to include
+//     // Authorization header with token value read from cookie
+//     if (config?.includeAuthorization) {
+//       config.withCredentials = true;
+//       if (isServer) {
+//         const { cookies } = await import("next/headers"),
+//           token = cookies().get(`${JWTAuthTokenName}`)?.value;
+
+//         if (token) {
+//           config.headers["Authorization"] = `Bearer ${token}`;
+//         }
+//       } else {
+//         const Cookies = await import("js-cookie"),
+//           token = Cookies.get(`${JWTAuthTokenName}`);
+//         // const token = document.cookie.replace(
+//         //   /(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/,
+//         //   "$1"
+//         // );
+
+//         if (token) {
+//           config.headers["Authorization"] = `Bearer ${token}`;
+//         }
+//       }
+//     }
+
+//     return config;
+//   },
+//   function (error) {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default http;
+
 http.interceptors.request.use(
   async (config) => {
-    // `includeAuthorization` is a custom config. It is checked to let axios know when we need to include
-    // Authorization header with token value read from cookie
     if (config?.includeAuthorization) {
       config.withCredentials = true;
       if (isServer) {
@@ -30,21 +64,31 @@ http.interceptors.request.use(
       } else {
         const Cookies = await import("js-cookie"),
           token = Cookies.get(`${JWTAuthTokenName}`);
-        // const token = document.cookie.replace(
-        //   /(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/,
-        //   "$1"
-        // );
 
         if (token) {
           config.headers["Authorization"] = `Bearer ${token}`;
         }
       }
     }
-
     return config;
   },
   function (error) {
     return Promise.reject(error);
+  }
+);
+
+// Add response interceptor
+http.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    let errorMessage = "";
+    if (error.response.data.statusCode == 401) {
+      errorMessage = error.response.data;
+    }
+
+    return Promise.reject(errorMessage);
   }
 );
 
