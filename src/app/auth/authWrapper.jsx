7f9e0e9c2a8d27dd382afd120d2eafd420228075
SHIRLEY-GITHINJI <shirley@ipayafrica.com>
2024-02-27 11:49:41 +0300

    style auth login

--- src/app/auth/authWrapper.jsx
+++ src/app/auth/authWrapper.jsx
@@ -1,12 +1,16 @@
-import { Stack, Typography } from "@mui/material";
+import Image from "next/image";
+
+import { Box, Stack } from "@mui/material";
+
+import ipaylogo from "@/assets/ipaylogo.svg";
 
 const AuthWrapper = ({ children }) => {
   return (
-    <Stack>
-      <Stack alignItems="center" py={2} borderBottom={1} borderColor="#DADEF0">
-        <Typography variant="h5">iPay Admin Dashboard</Typography>
-      </Stack>
-      <Stack alignItems="center" justifyContent="center" height="90vh">
+    <Stack sx={{ backgroundColor: "#F3F8FB" }}>
+      <Box px={5} py={3}>
+        <Image src={ipaylogo} alt="iPay logo" width={100} height={45} />
+      </Box>
+      <Stack alignItems="center" minHeight="85vh">
         {children}
       </Stack>
     </Stack>
